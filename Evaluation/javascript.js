37d322c949a185bc8b1b046f9aab1336cd1af489
Abadima <abadima06@gmail.com>
2022-03-13 11:19:45 -0400

    disable js/ts 4 safety

--- Evaluation/javascript.js
+++ Evaluation/javascript.js
@@ -8,26 +8,23 @@ if (isBuffer(process.argv[2])) {
 	console.error("No thanks, we value our system <3")
 }
 
-try {
-	var evaled = eval(process.argv[2]);
-	// const res =
-	//   typeof evaled === "string" ? evaled : inspect(evaled, {
-	//     depth: 0
-	//   });
-	const res = inspect(evaled, {
-		depth: 0
-	})
-	console.log(res);
-} catch (error) {
-	console.error(error)
-	//  if (error.toString() === "ReferenceError: evaled is not defined") {
-	//    try {
-	//      let results = eval(process.argv[2]);
-	//      console.log(results);
-	//    } catch (error) {
-	//      console.error(error);
-	//    }
-	//  } else {
-	//    console.error(error);
-	//  }
-}
+console.error("This endpoint is risky, we can't allow operations for now.")
+// try {
+//   var evaled = eval(process.argv[2]);
+//   const res =
+//     typeof evaled === "string" ? evaled : inspect(evaled, {
+//       depth: 0
+//     });
+//   console.log(res);
+// } catch (error) {
+//   if (error.toString() === "ReferenceError: evaled is not defined") {
+//     try {
+//       let results = eval(process.argv[2]);
+//       console.log(results);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   } else {
+//     console.error(error);
+//   }
+// }
